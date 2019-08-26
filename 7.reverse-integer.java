80d7be01affb26ca19e506f1ba93277c2aa83a41
zgq_ubuntu <zgq2015@foxmail.com>
2019-08-27 00:25:25 +0800

    leetcode

--- 7.reverse-integer.java
+++ 7.reverse-integer.java
@@ -5,30 +5,13 @@
  */
 class Solution {
     public int reverse(int x) {
-        System.out.print("" + Integer.MAX_VALUE + "\n");
-        if(x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) return 0;
-        int level = 0;
         int ret = 0;
-        int length = x > 0 ? ("" + x).length() : ("" + x).length() - 1;
-        boolean needAnafirst = true;
         while(x != 0) {
-            if(needAnafirst){
-                if(x % 10 != 0){
-                    needAnafirst = false;
-                }else{
-                    length--;
-                    x = x / 10;
-                    continue;
-                }
-            }
-            int tmp = 1;
-            for(int i = 0; i < length - level -1; i++){
-                tmp *= 10;
-            }
-            ret = ret + x % 10 * tmp;
-            if(ret > Integer.MAX_VALUE || ret < Integer.MIN_VALUE) return 0;
-            level++;
-            x = x / 10;
+            int pop = x % 10;
+            x = x/10;
+            if(ret > Integer.MAX_VALUE/10 || (ret == Integer.MAX_VALUE/10 && pop > 7)) return 0;
+            if(ret < Integer.MIN_VALUE/10 || (ret == Integer.MIN_VALUE/10 && pop < -8)) return 0;
+            ret = ret * 10 + pop;
         }
         return ret;
     }

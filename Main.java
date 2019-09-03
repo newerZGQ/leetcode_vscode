84189e082de084971412676bf8ffe0f6ea3e81fd
nanwei.zgq <nanwei.zgq@alibaba-inc.com>
2019-09-03 21:06:32 +0800

    leet

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args){
-        String ret = "" + new Solution().reverse(1534236469);
-        System.out.print("" + ret);
+    public static void main(String[] args) {
+        // String ret = "" + new Solution().reverse(1534236469);
+        System.out.print("sssss" + (-18 % 10) + "\n");
     }
 }
